a20d3cca192965356165db2c728be436765cb77d
Xolile Nxiweni <Xolile.Nxiweni@dynamicdna.co.za>
2023-11-03 12:07:47 +0200

    Second commit

--- js.js
+++ js.js
@@ -1,37 +1,3 @@
-// header functions
-const header = document.querySelector("nav");
-window.addEventListener ("scroll", function() {
-    header.classList.toggle ("sticky", window.scrollY > 100);
-});
-
-// let menu = document.querySelector('#menu-icon');
-// let navlist = document.querySelector('.links');
-
-window.onscroll = () => {
-    menu.classList.remove('tog');
-    navlist.classList.remove('open');
-};
-
-//card flip fucntion for menu
-var cards = document.querySelectorAll('.card');
-
-[...cards].forEach((card)=>{
-  card.addEventListener( 'click', function() {
-    card.classList.toggle('is-flipped');
-  });
-});
-//dropdown function for menu
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
+function contactPopUp(){
+  alert("Under Development!!")
 }
