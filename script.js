e394f8be629f1edb9199aafb3ff9fb5e6ce3f6d5
Xoli <xolinxiweni@gmail.com>
2023-11-05 20:37:22 +0200

    Updated Landing Page

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 function contactPopUp(){
   alert("Under Development!!")
+};
+
+//sticky scroll behavior
+const header = document.querySelector("nav");
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+//navsDisappear
+const checkbox = document.getElementById('nav-toggle');
+const navLinks = document.querySelectorAll('a');
+function closeMobileNav(){
+  checkbox.checked = false;
 }
+navLinks.forEach(link => {
+  link.addEventListener('click', closeMobileNav)
+})
+
+//wordchange effect
+var typed = new Typed(".effect", {
+  strings: ["Designer", "Developer", "Tester"],
+  loop: true,
+  typeSpeed: 75,
+  backSpeed: 5,
+  backDelay: 2500,
+});
+
+//Pop up contact page
+const showPopupButton = document.getElementById("showPopup");
+const closePopupButton = document.getElementById("closePopup");
+const contactPopup = document.getElementById("contactPopup");
+
+showPopupButton.addEventListener("click", function () {
+    contactPopup.style.display = "block";
+});
+
+closePopupButton.addEventListener("click", function () {
+    contactPopup.style.display = "none";
+});
